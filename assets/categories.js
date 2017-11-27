2766d55f8d0c589b111748ff8622370dca7d6243
Melanie MacDonald <melmacd94@gmail.com>
2017-11-27 13:26:39 -0500

    topicsArray

---
+++ assets/categories.js
@@ -0,0 +1,221 @@
+var topicsArray = [
+    {walmart:"Arts, Crafts & Sewing",
+    amazon: {
+        SearchIndex: "ArtsAndCrafts",
+        Keywords:"ArtsAndCrafts",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Auto & Tires",
+    amazon: {
+        SearchIndex: "Automotive",
+        Keywords:"Automotive",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Baby",
+    amazon: {
+        SearchIndex: "Baby",
+        Keywords:"Baby",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Beauty",
+    amazon: {
+        SearchIndex: "Beauty",
+        Keywords:"Beauty",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Books",
+    amazon: {
+        SearchIndex: "Books",
+        Keywords:"Books",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+       {walmart:"Cell Phones",
+    amazon: {
+        SearchIndex: "Electronics",
+        Keywords:"CellPhones",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Clothing",
+    amazon: {
+        SearchIndex: "Fashion",
+        Keywords:"Fashion",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Electronics",
+    amazon: {
+        SearchIndex: "Electronics",
+        Keywords:"Electronics",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Food",
+    amazon: {
+        SearchIndex: "Grocery",
+        Keywords:"Grocery",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Gifts & Registry",
+    amazon: {
+        SearchIndex: "GiftCards",
+        Keywords:"GiftCards",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },   {walmart:"Health",
+    amazon: {
+        SearchIndex: "HealthPersonalCare",
+        Keywords:"HealthPersonalCare",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Home",
+    amazon: {
+        SearchIndex: "HomeGarden",
+        Keywords:"HomeGarden",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Home Improvement",
+    amazon: {
+        SearchIndex: "LawnAndGarden",
+        Keywords:"LawnAndGarden",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Household Essentials",
+    amazon: {
+        SearchIndex: "HomeGarden",
+        Keywords:"HomeGarden",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Industrial & Scientific",
+    amazon: {
+        SearchIndex: "Industrial",
+        Keywords:"Industrial",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },   {walmart:"Jewelry",
+    amazon: {
+        SearchIndex: "Fashion",
+        Keywords:"Jewelry",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Movies & TV",
+    amazon: {
+        SearchIndex: "Movies",
+        Keywords:"Movies",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Music on CD or Vinyl",
+    amazon: {
+        SearchIndex: "Music",
+        Keywords:"Music",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Musical Instruments",
+    amazon: {
+        SearchIndex: "MusicalInstruments",
+        Keywords:"MusicalInstruments",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Office",
+    amazon: {
+        SearchIndex: "OfficeProducts",
+        Keywords:"OfficeProducts",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Party & Occasions",
+    amazon: {
+        SearchIndex: "Fashion",
+        Keywords:"PartySupplies",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Patio & Garden",
+    amazon: {
+        SearchIndex: "LawnAndGarden",
+        Keywords:"LawnAndGarden",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Personal Care",
+    amazon: {
+        SearchIndex: "Beauty",
+        Keywords:"PersonalCare",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Pets",
+    amazon: {
+        SearchIndex: "PetSupplies",
+        Keywords:"PetSupplies",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },   {walmart:"Photo Center",
+    amazon: {
+        SearchIndex: "All",
+        Keywords:"Photographs",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+     {walmart:"Seasonal",
+    amazon: {
+        SearchIndex: "HomeGarden",
+        Keywords:"SeasonalDecorations",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Services",
+    amazon: {
+        SearchIndex: "All",
+        Keywords:"All",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Sports & Outdoors",
+    amazon: {
+        SearchIndex: "SportingGoods",
+        Keywords:"SportingGoods",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },  {walmart:"Toys",
+    amazon: {
+        SearchIndex: "Toys",
+        Keywords:"Toys",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    },
+    },  {walmart:"Video Games",
+    amazon: {
+        SearchIndex: "VideoGames",
+        Keywords:"VideoGames",
+        sort: "salesrank",
+        ResponseGroup:"ItemAttributes,Offers"
+    }
+    }
+]
