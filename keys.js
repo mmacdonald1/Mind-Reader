c86d7755580ecddd978fe247994a14f35c58707b
Melanie MacDonald <melmacd94@gmail.com>
2017-11-28 12:25:06 -0500

    keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+var walmartKey = 'hbnhxegbe6tutrap33qgwt2c'
+var awsId = 'AKIAJXVDU57RIXD43QDA'
+var awsSecret = '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'
+var assocId = 'mmacdonald1-20'
+module.exports = {
+  walmartKey,
+  awsId,
+  awsSecret,
+  assocId,
+}
