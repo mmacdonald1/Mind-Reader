a519054f2a597f82e1b97d71a745ca179e86999f
fitsum belayneh <fitsu_alt@yahoo.com>
2020-01-15 10:45:27 -0500

    walmart and amazon search bar position changed

--- assets/js/amazon-search.js
+++ assets/js/amazon-search.js
@@ -1,33 +1,49 @@
-$(document).ready(function() {
-    $('#submitbtn-amz').on('click', function() {
-        var searchTerm = $('#query-amz').val();
-        var requestUrl = '/api/products/amazon?query='+ searchTerm;
-        $.getJSON(requestUrl, function(data) {
-            console.log(data)
-            data.forEach(function(item) {
-                var productHtml = '';
-                var itemId = item.ASIN;
-                var itemName = item.ItemAttributes.Title;
-                var itemImage =  item.MediumImage.URL;
-                var itemPrice;
+//Displaying Walmart Search Bar
+const walmartButton = document.querySelector("#walmart-button");
+const walmartInput = document.querySelector("#walmart-input");
+const amazonButton = document.querySelector("#amazon-button");
+const amazonInput = document.querySelector("#collapseExample");
 
-                if(item.Offers.TotalOffers == '0') {
-                    // Because it is unavailable
-                    itemPrice = '$0.00';
-                } else {
-                    itemPrice = item.ItemAttributes.ListPrice.FormattedPrice;                    
-                }
-                productHtml += `
+walmartButton.addEventListener("click", () => {
+  walmartInput.style.display = "block";
+  amazonInput.style.display = "none";
+});
+
+amazonButton.addEventListener("click", () => {
+  amazonInput.style.display = "block";
+  walmartInput.style.display = "none";
+});
+
+$(document).ready(function() {
+  $("#submitbtn-amz").on("click", function() {
+    var searchTerm = $("#query-amz").val();
+    var requestUrl = "/api/products/amazon?query=" + searchTerm;
+    $.getJSON(requestUrl, function(data) {
+      console.log(data);
+      data.forEach(function(item) {
+        var productHtml = "";
+        var itemId = item.ASIN;
+        var itemName = item.ItemAttributes.Title;
+        var itemImage = item.MediumImage.URL;
+        var itemPrice;
+
+        if (item.Offers.TotalOffers == "0") {
+          // Because it is unavailable
+          itemPrice = "$0.00";
+        } else {
+          itemPrice = item.ItemAttributes.ListPrice.FormattedPrice;
+        }
+        productHtml += `
                     <div class="amazon-product">
                         <img id="img-${itemId}" src="">
                         <p> Name: ${itemName} </p>
                         <p> Price: ${itemPrice} </p> 
                     </div>
                 `;
-                
-                $('#results').append(productHtml);
-                $(`#img-${itemId}`).attr('src', itemImage);
-            });
-        });
+
+        $("#results").append(productHtml);
+        $(`#img-${itemId}`).attr("src", itemImage);
+      });
     });
+  });
 });
--- assets/js/mind-reader.js
+++ assets/js/mind-reader.js
@@ -1,63 +1,58 @@
 $(document).ready(function() {
-  $.getJSON('/api/categories', function(data) {
-    var checkBoxesHTML = '';
-    data.forEach(function (category) {
-      var checkBox =
-              `<div>
+  $.getJSON("/api/categories", function(data) {
+    var checkBoxesHTML = "";
+    data.forEach(function(category) {
+      var checkBox = `<div>
                 <input type="checkbox" class="options checkbox" name="catId" value="${category.name}-${category.id}">
                 <label for="electronics-div">${category.name}</label>
               </div>`;
       checkBoxesHTML += checkBox;
     });
 
-    $('#cat-form').html(checkBoxesHTML);
+    $("#cat-form").html(checkBoxesHTML);
   });
 
-  $('#submit-btn').on('click', function() {
-      
-
-      //Hides loader after success icon appears
-    const hideLoader = (loaderEl) => {
-      Velocity(loaderEl, 'transition.fadeOut', 1000);
+  $("#submit-btn").on("click", function() {
+    //Hides loader after success icon appears
+    const hideLoader = loaderEl => {
+      Velocity(loaderEl, "transition.fadeOut", 1000);
     };
 
     //hides form after successful submission
     const hideForm = (formEl, iconEl, loaderEl) => {
-      Velocity(formEl,'transition.bounceUpOut', 500);
-      Velocity(loaderEl, 'transition.fadeIn');
+      Velocity(formEl, "transition.bounceUpOut", 500);
+      Velocity(loaderEl, "transition.fadeIn");
       //Simulate the time it takes to process the form
-      const timer = setTimeout( () => {
-       Velocity(iconEl, 'transition.bounceDownIn', 500);
+      const timer = setTimeout(() => {
+        Velocity(iconEl, "transition.bounceDownIn", 500);
       }, 2000);
       hideLoader(loaderEl);
     };
 
     //Caching reference to elements
-    const signButton = document.querySelector('#submit-btn'),
-          signForm = document.querySelector('.optionsList'),
-          success = document.querySelector('.sign-up__success'),
-          loader = document.querySelector('.sign-up__loader');
+    const signButton = document.querySelector("#submit-btn"),
+      signForm = document.querySelector(".optionsList"),
+      success = document.querySelector(".sign-up__success"),
+      loader = document.querySelector(".sign-up__loader");
 
     //Handler for sign up button
-    signButton.addEventListener('click', (e) => {
+    signButton.addEventListener("click", e => {
       e.preventDefault();
-      hideForm(signForm, success, loader); 
+      hideForm(signForm, success, loader);
     });
-      
-      setTimeout(function(){
-                
-        var checkedCategories = [];
-        Object.values($('.checkbox')).forEach(function(checkboxElement) {
-          // console.log(checkboxElement);
-          if(checkboxElement.checked) {
-            checkedCategories.push($(checkboxElement).val());
-          }
-        });
 
+    setTimeout(function() {
+      var checkedCategories = [];
+      Object.values($(".checkbox")).forEach(function(checkboxElement) {
+        // console.log(checkboxElement);
+        if (checkboxElement.checked) {
+          checkedCategories.push($(checkboxElement).val());
+        }
+      });
 
-        var searchUrl = '/searchs?cats=' + checkedCategories.join('|');
+      var searchUrl = "/searchs?cats=" + checkedCategories.join("|");
 
-        window.location.href = searchUrl;
-      },2800)
+      window.location.href = searchUrl;
+    }, 2800);
   });
 });
